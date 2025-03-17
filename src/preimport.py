3a8391cafedec89dfc8e1e04110d8254a09af146
RayZh <rayzhangshanghai@hotmail.com>
2025-03-17 18:55:18 +0800

    style: format preimport.py

--- src/preimport.py
+++ src/preimport.py
@@ -1,4 +1,4 @@
-import random;
+import random
 import re
 from math import *
 
@@ -11,22 +11,22 @@ def rdouble(a, b):
 def expand(matched):
     st = ord(matched.group(1))
     ed = ord(matched.group(2))
-    str = ""
-    for c in range(st,ed+1):
-        str+=chr(c)
+    str = ''
+    for c in range(st, ed + 1):
+        str += chr(c)
     return str
 
-def rstr(chars:str, len:int):
-    string = ""
-    real_chars = ""
-    if chars.find("\\-") != -1:
-        real_chars += "-"
-        chars = chars.replace("\\-", "")
+def rstr(chars: str, len: int):
+    string = ''
+    real_chars = ''
+    if chars.find('\\-') != -1:
+        real_chars += '-'
+        chars = chars.replace('\\-', '')
 
-    real_chars += re.sub("(.)-(.)", expand, chars)
+    real_chars += re.sub('(.)-(.)', expand, chars)
     
     for i in range(len):
-        string+=random.choice(real_chars)
+        string += random.choice(real_chars)
 
     return string
 
